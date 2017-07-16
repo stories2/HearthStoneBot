20258134d3f45c39a09fd345a7329cba4fa69e6a
stories2 <stories2@naver.com>
2017-07-16 12:33:35 +0900

    File io module : load file message data using tail order

--- Core/FileIO.py
+++ Core/FileIO.py
@@ -1,5 +1,17 @@
 from Settings import DefineManager
 from Utils import LogManager
+import subprocess
 
 def RealtimeLoader(targetFilePath = DefineManager.DEFAULT_LOG_FILE_SAVED_PATH):
     LogManager.PrintLog("FileIO", "RealtimeLoader", "Load file path: " + targetFilePath, DefineManager.LOG_LEVEL_INFO)
+
+    hearthStoneLogFile = subprocess.Popen(['tail', '-F', targetFilePath], stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+
+    while True:
+        logMessage = hearthStoneLogFile.stdout.readline()
+
+        if not logMessage:
+            LogManager.PrintLog("FileIO", "RealtimeLoader", "File read process ended", DefineManager.LOG_LEVEL_INFO)
+
+        else:
+            LogManager.PrintLog("FileIO", "RealtimeLoader", logMessage, DefineManager.LOG_LEVEL_INFO)
