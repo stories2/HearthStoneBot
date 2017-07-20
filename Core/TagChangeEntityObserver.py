a9abea8a2b010ce999a32e3f3f0da7538fbd51cd
stories2 <stories2@naver.com>
2017-07-20 13:40:07 +0900

    Observer module : save field cards status

--- Core/TagChangeEntityObserver.py
+++ Core/TagChangeEntityObserver.py
@@ -2,6 +2,8 @@ from Settings import DefineManager
 from Utils import LogManager
 import re
 
+fieldCardsInfo = {}
+
 def DetectGameStatus(logMessage):
     tagChangeEntityData = re.search("TAG_CHANGE Entity=(.+?) tag=PLAYSTATE value=(.+?)\n", logMessage)
 
@@ -27,17 +29,34 @@ def IsGameStart(logMessage):
     return False
 
 def DetectFieldCard(logMessage):
+    global fieldCardsInfo
+
     tagChangeEntityData = re.search("TAG_CHANGE Entity=(.+?) tag=NUM_TURNS_IN_PLAY value=(.+?)\n", logMessage)
     if tagChangeEntityData != None:
         LogManager.PrintLog("TagChangeEntityObserver", "DetectFieldCard", "entity: " + tagChangeEntityData.group(1) +
                             " value: " + tagChangeEntityData.group(2), DefineManager.LOG_LEVEL_INFO)
 
+        entityDetailData = re.search("name=(.+?) id=(.+?) zone=(.+?) zonePos=(.+?) cardId=(.+?) player=(.+?)", tagChangeEntityData.group(1))
+
+        if entityDetailData != None:
+
+            entityDetailArray = [entityDetailData.group(1), entityDetailData.group(2), entityDetailData.group(3),
+                                 entityDetailData.group(4), entityDetailData.group(5), entityDetailData.group(6)]
+
+            # (player number - 1) * limit of field card num + card field position
+            fieldCardsInfoSavePoint = (int(entityDetailArray[5]) - 1) * 7 + int(entityDetailArray[3])
+
+            fieldCardsInfo[fieldCardsInfoSavePoint] = [entityDetailArray, tagChangeEntityData.group(2)]
+
 def DetectTurns(logMessage):
+    global fieldCardsInfo
+
     tagChangeEntityData = re.search("TAG_CHANGE Entity=(.+?) tag=STEP value=(.+?)\n", logMessage)
     if tagChangeEntityData != None:
         LogManager.PrintLog("TagChangeEntityObserver", "DetectTurns", "entity: " + tagChangeEntityData.group(1) +
                             " value: " + tagChangeEntityData.group(2), DefineManager.LOG_LEVEL_INFO)
         if tagChangeEntityData.group(2) == "MAIN_READY":
+            fieldCardsInfo = {}
             LogManager.PrintLog("TagChangeEntityObserver", "DetectTurns", "Print field status", DefineManager.LOG_LEVEL_INFO)
         elif tagChangeEntityData.group(2) == "MAIN_START_TRIGGERS":
             LogManager.PrintLog("TagChangeEntityObserver", "DetectTurns", "MAIN_START_TRIGGERS", DefineManager.LOG_LEVEL_INFO)
