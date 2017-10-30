bc89a811e9410a5c1944a7586760ed7ecd6a3fc6
stories2 <stories2@naver.com>
2017-10-30 22:06:53 +0900

    Core/FieldHelper module : getting best each player card swap

--- Core/FieldHelper.py
+++ Core/FieldHelper.py
@@ -5,4 +5,12 @@ def CalculateProcess(fieldData):
     LogManager.PrintLog("FieldHelper",
                         "CalculateProcess", "player field data accepted",
                         DefineManager.LOG_LEVEL_INFO)
+    BestCardSwap(fieldData, 0)
+    BestCardSwap(fieldData, 1)
+    return
+
+def BestCardSwap(fieldData, playerNumber):
+    LogManager.PrintLog("FieldHelper",
+                        "BestCardSwap", "calculate best card swap player : " + playerNumber,
+                        DefineManager.LOG_LEVEL_INFO)
     return
